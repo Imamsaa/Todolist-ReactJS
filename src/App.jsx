c694d1bb2708bc4de52206d491076264f8c90d6a
Imamsaa <imamsafii2222022@gmail.com>
2026-09-08 01:01:52 +0700

    Feat : First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Form from "./components/form"
+import Todolist from "./components/Todolist"
+function App() {
+  return (
+    <>
+      <Form/>
+      <Todolist/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodolistButton.jsx
@@ -0,0 +1,11 @@
+function TodolistButton() {
+  return (
+    <>
+        <span><button>👆</button></span>
+        <span><button>👇</button></span>
+        <span><button>🗑️</button></span>
+    </>
+  )
+}
+
+export default TodolistButton
